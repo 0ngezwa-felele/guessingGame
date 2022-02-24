8beb09de72d1acf7feb7bde236eacc1a1b347c3b
Ongezwa <ongezwaf27@gmail.com>
2022-02-24 12:57:42 +0200

    style and index file

--- guess.js
+++ guess.js
@@ -7,83 +7,30 @@ const inputBox = document.querySelector('.inputBox')
 
 
 function guessingGame() {
-	const num = Number(inputBox.value);
+    const num = Number(inputBox.value);
 
-	if (num < randomNumber) {
-		fieldSpace.innerHTML = "Your guess is too low";
-	} else if (num > randomNumber) {
-		fieldSpace.innerHTML = "Your guess is too high";
-	} else {
-		fieldSpace.innerHTML =  `Correct, the secret number is ${num}`;
-	}
+    if (num < randomNumber) {
+        fieldSpace.innerHTML = "Your guess is too low";
+    } else if (num > randomNumber) {
+        fieldSpace.innerHTML = "Your guess is too high";
+    } else {
+        fieldSpace.innerHTML = `Correct, the secret number is ${num}`;
+    }
 
 
-	setTimeout(function(){
-		display.innerHTML = "";
-	}, 2000);
+    setTimeout(function () {
+        display.innerHTML = "";
+    }, 2000);
 
 
 }
 
+function timeOut() {
+    fieldSpace.innerHTML = "Game started!"
+    randomNumber = Math.ceil((Math.random() * 100))
+}
+
+
 btn.addEventListener('click', guessingGame)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var hello = Guessing()
-
-// var numberInput = textBoxElement.value
-
-// let randomNumber = Math.ceil((Math.random() * 100))
-
-
-// if (numberInput <= 50) {
-//     displayElement.innerHTML = hello.count();
-
-// }
-
-
-
-
-
-
-// buttonElement.addEventListener('click', checked);
